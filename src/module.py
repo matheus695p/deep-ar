6479ac80fc1054cfec7f6b1ee12955799406d5c8
matheus695p <mateu_695p@hotmail.com>
2021-03-28 16:30:36 -0300

    add: tests de estacionaridad

--- src/module.py
+++ src/module.py
@@ -1,6 +1,10 @@
 import os
+import warnings
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def plot_time_series(df, fecha_inicial="2014-12-01",
@@ -187,3 +191,141 @@ def df_convert_float(df):
             print(e)
     df.reset_index(drop=True, inplace=True)
     return df
+
+
+def comentary_stationarity(p):
+    """
+    Comentario de la estacionaridad de las
+
+    Parameters
+    ----------
+    p : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    comments : TYPE
+        DESCRIPTION.
+
+    """
+    if p <= 0.05:
+        comments =\
+            "Rechaza la hipótesis nula (H0), los datos son estacionarios"
+    else:
+        comments =\
+            "No se rechaza la hipótesis nula (H0), los datos no son estacionarios"
+    return comments
+
+
+def creating_rolling_dataset(data, mean_rolling=30):
+    """
+    Entregado un dataset, crear el mismo dataset pero a través de medias
+    moviles, de tal forma de generar predicciones en ella
+    Parameters
+    ----------
+    data : dataframe
+        dataframe con las demandas.
+    mean_rolling : int, optional
+        tamaño de la ventana temporal para hacer las medias moviles.
+        The default is 30.
+    Returns
+    -------
+    result : dataframe
+        dataframe de medias moviles de data.
+    """
+    result = data[["fecha"]]
+    # columnas a iterar sobre
+    columns = list(data.columns)
+    columns.remove("fecha")
+    for col in columns:
+        col_name = f"rolling_{col}"
+        datai = data[["fecha", col]]
+        datai.set_index("fecha", inplace=True)
+        datai[col_name] = datai[col].rolling(mean_rolling).mean()
+        datai = datai[[col_name]]
+        datai.reset_index(drop=False, inplace=True)
+        result = result.merge(datai, on=["fecha"], how="outer")
+    result.dropna(inplace=True)
+    result.reset_index(drop=True, inplace=True)
+    return result
+
+
+def index_date(df):
+    """
+    Indexar la columna de las fechas
+    Parameters
+    ----------
+    df : dataframe
+        dataframe de demandas de stock en el tiempo.
+    Returns
+    -------
+    df : dataframe
+        dataframe con la fecha como index.
+    """
+    df.rename(columns={"fecha": "index"}, inplace=True)
+    df.set_index(["index"], inplace=True)
+    return df
+
+
+def stationary_adf_test(df):
+    """
+    Test de estacionaridad
+     Augmented Dickey-Fuller puede ser una de las más utilizadas.
+     Utiliza un modelo autorregresivo y optimiza un criterio de información
+     a través de múltiples valores de retardo (lags) diferentes. La hipótesis
+     nula de la prueba es que la serie de tiempo se puede representar
+     mediante una raíz unitaria, que no es estacionaria (tiene alguna
+     estructura dependiente del tiempo). La hipótesis alternativa
+     (que rechaza la hipótesis nula) es que la serie de tiempo es
+     estacionaria.
+
+    * Hipótesis nula (H0): si no se rechaza, sugiere que la serie de tiempo
+    tiene una raíz unitaria, lo que significa que no es estacionaria.
+    Tiene alguna estructura dependiente del tiempo --> problemas.
+    * Hipótesis alternativa (H1): Se rechaza la hipótesis nula; sugiere que
+    la serie de tiempo no tiene una raíz unitaria, lo que significa que es
+    estacionaria. No tiene una estructura dependiente del tiempo.
+
+    Voy a interpretar este resultado utilizando el valor p de la prueba.
+    Un valor p por debajo de un umbral (como 5% o 1%) sugiere que rechazamos
+    la hipótesis nula (estacionaria); de lo contrario, un valor p por encima
+    del umbral sugiere que no rechazamos la hipótesis nula (no estacionaria),
+    este es el lo clásico en tests estadísticos.
+
+    * Valor p> 0.05: No se rechaza la hipótesis nula (H0),
+        los datos no son estacionarios
+    * Valor de p <= 0.05: Rechaza la hipótesis nula (H0),
+        los datos son estacionarios
+
+    Parameters
+    ----------
+    df : dataframe
+        dataframe al cual se le harán pruebas de estacionaridad a todas sus
+        columnas.
+    Returns
+    -------
+    output : dataframe
+        resultados del test.
+    """
+    output = []
+    for col in df.columns:
+        if "fecha" in col:
+            pass
+        else:
+            datai = df[[col]]
+            # datai = datai[datai[col] > 0]
+            result = adfuller(datai.values)
+            # descomprimir valores
+            p = result[1]
+            print("Para la columna: ", col)
+            print(comentary_stationarity(p))
+            print('ADF estadisticas: %f' % result[0])
+            print('Valor de p: %f' % result[1])
+            print('Valores criticos:')
+            for key, value in result[4].items():
+                print('\t%s: %.3f' % (key, value))
+            output.append([col, comentary_stationarity(p),
+                          p, list(result[4].items())])
+    output = pd.DataFrame(output, columns=["columna", "resultado",
+                                           "valor_p", "intervalos"])
+    return output
